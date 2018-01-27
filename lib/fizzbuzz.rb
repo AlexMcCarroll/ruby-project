3733fced8627b27bc872dd5bbe31d1ae881ba21e
Cindy <jl7e12@gmail.com>
2018-01-27 14:41:31 +0000

    refactoring

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,8 +1,6 @@
 def fizzbuzz(n)
 	return "fizzbuzz" if n%3==0 && n%5==0
-	if (n%3 ==0 || n%5 ==0)
-		n%3==0 ? "fizz" : "buzz"
-	else
-		return n
-	end
+	return "fizz" if n%3==0
+	return "buzz" if n%5==0
+	  n
 end
